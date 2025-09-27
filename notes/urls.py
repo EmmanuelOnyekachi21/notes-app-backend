4e2e149cdb83535c9a5b962e25feb23f1d23e0d7
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2025-09-27 09:01:21 +0100

    Add missing routes for categories and create_note in URLs

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,6 @@ from notes import views
 urlpatterns = [
     path("notes/", views.note_list, name="notes"),
     path("notes/<slug:slug>/", views.note_detail, name="note-detail"),
+    path('categories/', views.categorys, name='categorys'),
+    path('create/', views.create_note, name='create-note'),
 ]
